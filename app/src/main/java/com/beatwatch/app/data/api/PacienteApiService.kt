1cab5ad2779dc1cec78b1377235d863e0b9471ec
KICHOC05 <christophercamargog05@gmail.com>
2026-08-03 18:55:20 -0600

    feat(perfil): agregar edicion de datos

--- app/src/main/java/com/beatwatch/app/data/api/PacienteApiService.kt
+++ app/src/main/java/com/beatwatch/app/data/api/PacienteApiService.kt
@@ -1,13 +1,16 @@
 package com.beatwatch.app.data.api
 
+import com.beatwatch.app.data.model.ActualizarPacienteRequest
 import com.beatwatch.app.data.model.PacientePerfilResponse
 import com.beatwatch.app.data.model.RegistroPacienteRequest
 import com.beatwatch.app.data.model.RegistroPacienteResponse
+import okhttp3.ResponseBody
 import retrofit2.Response
 import retrofit2.http.Body
 import retrofit2.http.GET
 import retrofit2.http.Header
 import retrofit2.http.Headers
+import retrofit2.http.PATCH
 import retrofit2.http.POST
 import retrofit2.http.Path
 
@@ -32,4 +35,15 @@ interface PacienteApiService {
         @Header("Authorization") authorization: String,
         @Path("usuarioId") usuarioId: String
     ): Response<PacientePerfilResponse>
+
+    @Headers(
+        "Content-Type: application/json",
+        "Accept: application/json"
+    )
+    @PATCH("api/Pacientes/perfil/{usuarioId}")
+    suspend fun actualizarPerfilPaciente(
+        @Header("Authorization") authorization: String,
+        @Path("usuarioId") usuarioId: String,
+        @Body request: ActualizarPacienteRequest
+    ): Response<ResponseBody>
 }
